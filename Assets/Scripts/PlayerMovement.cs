a69d43bb591681891aec85be59bad974f85ee5ba
Дмитрий Чирков <dschirkov@edu.hse.ru>
2020-07-22 22:50:11 +0500

    Study RPG

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Walk,
+    Attack,
+    Interact
+}
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        currentState = PlayerState.Walk;
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("MoveY", 0);
+        animator.SetFloat("MoveX", -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (Input.GetButton("Attack") && (currentState != PlayerState.Attack))
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.Walk)
+        {
+            UpdateAnimationsAndMove();
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("Attacking", true);
+        currentState = PlayerState.Attack;
+        yield return null;
+        animator.SetBool("Attacking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.Walk;
+    }
+    void UpdateAnimationsAndMove()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+            animator.SetBool("Moving", false);
+    }
+
+    void MoveCharacter()
+    {
+        change.Normalize();
+        myRigidbody.MovePosition(
+                transform.position + change * speed * Time.deltaTime
+            );
+    }
+
+}
---
+++ Assets/Scripts/Sign1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sign1 : MonoBehaviour
+{
+    public GameObject dialogBox1;
+    public Text dialogText1;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            if (dialogBox1.activeInHierarchy)
+            {
+                dialogBox1.SetActive(false);
+            }
+            else
+            {
+                dialogBox1.SetActive(true);
+                dialogText1.text = dialog;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogBox1.SetActive(false);
+        }
+    }
+}
